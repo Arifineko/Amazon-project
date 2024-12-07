f9133adf80dc81d1d5f2e34661c70753f403664f
ArifinEko <aeko86869@gmail.com>
2024-12-07 11:02:23 +0700

    feat: localstorage and remove products from cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
 },
@@ -6,6 +6,11 @@ export const cart = [{
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1
 }];
+
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -27,5 +32,15 @@ export function addToCart(productId) {
             quantity
         })
     }
+
+    saveToStorage()
 }
 
+
+export function removeFromCart(productId) {
+    const newCart = cart.filter(cartItem => cartItem.productId !== productId);
+
+    cart = newCart
+
+    saveToStorage()
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 
 let cartItemHTML = ""
@@ -12,7 +13,7 @@ cart.forEach(cartItem => {
     const matchingProduct = products.find(product => product.id === productId);
 
     cartItemHTML += `
-     <div class="cart-item-container">
+     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -26,7 +27,7 @@ cart.forEach(cartItem => {
                 ${matchingProduct.name}
             </div>
             <div class="product-price">
-                $${matchingProduct.priceCents / 100}
+                $${formatCurrency(matchingProduct.priceCents)}
             </div>
             <div class="product-quantity">
                 <span>
@@ -35,7 +36,9 @@ cart.forEach(cartItem => {
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-quantity-link"
+                data-product-id="${matchingProduct.id}"
+                >
                 Delete
                 </span>
             </div>
@@ -93,3 +96,13 @@ cart.forEach(cartItem => {
 
 document.querySelector('.js-order-summary')
     .innerHTML = cartItemHTML;
+
+document.querySelectorAll('.js-delete-quantity-link')
+    .forEach(link => link.addEventListener('click', () => {
+        const { productId } = link.dataset
+
+        removeFromCart(productId)
+
+        document.querySelector(`.js-cart-item-container-${productId}`)
+            .remove()
+    }))
